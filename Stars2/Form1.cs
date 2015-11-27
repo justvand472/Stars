ecd646f353e864bbf6815919d6a5d40e4c467e3d
justvand472 <justvand472@ed.amdsb.ca>
2015-11-27 14:31:20 -0500

    all done

--- Stars2/Form1.cs
+++ Stars2/Form1.cs
@@ -12,9 +12,41 @@ namespace Stars2
 {
     public partial class Form1 : Form
     {
+        public static Random randNum = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void StarField(int stars)
+        {
+            Graphics gpix = this.CreateGraphics();
+            SolidBrush brush = new SolidBrush(Color.White);
+            for (int i = 0; i <= stars; i++)
+            {                
+                int x, y, w, r, g, b;
+
+                x = randNum.Next(1, this.Width);
+                y = randNum.Next(1, this.Height);
+                w = randNum.Next(1, 11);
+                r = randNum.Next(0, 256);
+                g = randNum.Next(0, 256);
+                b = randNum.Next(0, 256);
+
+                brush.Color = Color.FromArgb(r, g, b);
+
+                gpix.FillEllipse(brush, x, y, w, w);
+            }
+            
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            Graphics gpix = this.CreateGraphics();
+            gpix.Clear(Color.White);
+            StarField(1000);
+            
         }
     }
 }
